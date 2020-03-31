f36c5496c43afc8096e78bce49743216849dc3e3
Ivan5 <converse_ivan@hotmail.com>
2020-03-31 14:05:14 -0600

    Make register user and store in DB

--- register.php
+++ register.php
@@ -5,12 +5,32 @@ if (isset($_POST['register'])) {
   //$userName = $_POST['userName'];
   //$userEmail = $_POST['userEmail'];
   //$password = $_POST['password'];
+  //Sanitize all data
   $userName = filter_var($_POST['userName'], FILTER_SANITIZE_STRING);
   $userEmail = filter_var($_POST['userEmail'], FILTER_SANITIZE_EMAIL);
   $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+  //Has a password
+  $passwordHas = password_hash($password, PASSWORD_DEFAULT);
 
+  //verify if the email data is a valid email
   if (filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
-    echo $userEmail . " " . $userName . " " . $password . " ";
+    //Get all information if of the user table only if the email match with the email in the store
+    $stmt = $pdo->prepare('SELECT * from users WHERE email = ?');
+    $stmt->execute([$userEmail]);
+    //get the number of rows
+    $totalUsers = $stmt->rowCount();
+
+    //If the totalUsers is more than zero is because the email is now in the store
+    if ($totalUsers > 0) {
+      //You can insert the data because the emial attribute is ready in use in the db
+      //echo "Email already been taken <br>";
+      $emailTaken = "Email already been taken <br>";
+    } else {
+      //if not you can inset the data
+      $stmt = $pdo->prepare('INSERT INTO users(name,email,password) VALUES(?,?,?)');
+      $stmt->execute([$userName, $userEmail, $passwordHas]);
+    }
+    //echo $totalUsers . '<br>';
   }
 
   //echo $userName . ' ' . $userEmail . ' ' . $password;
@@ -33,6 +53,10 @@ if (isset($_POST['register'])) {
         <div class="form-group">
           <label for="userEmail">Email</label>
           <input type="email" name="userEmail" class="form-control" required>
+          <br>
+          <?php if (isset($emailTaken)) { ?>
+            <p class="alert alert-danger"><?php echo $emailTaken ?></p>
+          <?php } ?>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
